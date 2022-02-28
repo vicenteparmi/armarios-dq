3e578ea860c384c1337b0e9a8633cac873741331
Vicente Parmigiani <vicenteparmigiani@gmail.com>
2022-02-28 15:45:51 -0300

    Base design

--- index.php
+++ index.php
@@ -1,14 +1,75 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Armários DQ</title>
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,700&amp;display=swap&amp;subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
 </head>
+
 <body>
     <?php require 'modules/header.php' ?>
 
-    <script src="firebase.js"></script>
+    <div class="info-pannel-container">
+        <div class="info-pannel">
+            <span>Seu armário</span>
+            <div id="locker">NI</div>
+            <h3 style="margin: 0;">Vicente K Parmigiani</h3>
+            <p>Status: Regular
+                <br>
+                Contrato feito em 20/02/2020
+            </p>
+            <button>Gerenciar Armários</button>
+        </div>
+
+        <div class="info-table">
+            <table class="view-table">
+                <tr>
+                    <th>Número</th>
+                    <th>Situação</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Regular</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Regular</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Irregular</td>
+                </tr>
+            </table>
+        </div>
+        <div class="info-table">
+            <table class="view-table">
+                <tr>
+                    <th>Número</th>
+                    <th>Situação</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Regular</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Regular</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Irregular</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+
+    <script type="module" src="firebase.js"></script>
 </body>
+
 </html>
--- modules/header.php
+++ modules/header.php
@@ -1,12 +1,61 @@
 <?php
-    $activePage = basename($_SERVER['PHP_SELF'], ".php");
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
 ?>
+<style>
+    nav {
+        background-color: #fff;
+        border-bottom: 1px solid #ccc;
+        padding: 0;
+        margin: 0;
+        display: inline-flex;
+        align-content: center;
+        width: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
 
+    .links {
+        display: inline-flex;
+        align-items: center;
+    }
+
+    .links>li {
+        padding: 0px 12px 0px 12px;
+        list-style: none;
+    }
+
+    .links>li>a {
+        color: black;
+        text-decoration: none;
+    }
+
+    .links>li.active>a {
+        background-color: var(--primary);
+        padding: 8px 12px 8px 12px;
+        color: white;
+        font-weight: bold;
+        border-radius: 100px;
+    }
+
+    #pageTitle {
+        font-size: medium;
+        font-weight: bold;
+        display: inline-flex;
+        margin-left: 5vw;
+    }
+</style>
 <nav>
-    <h2>Armários do Departamento de Química</h2>
-    <ul>
-        <li <?php if ($activePage == "index.php") { echo 'class="active"'; } ?>><a href="index.php">Início</a></li>
-        <li <?php if ($activePage == "meuscontratos.php") { echo 'class="active"'; } ?>><a href="meuscontratos.php">Meus Contratos</a></li>
-        <li <?php if ($activePage == "help.php") { echo 'class="active"'; } ?>><a href="help.php">Como reservar</a></li>
+    <h2 id='pageTitle'>Armários do<br>Departamento de Química</h2>
+    <ul class="links">
+        <li <?php if ($activePage == "index") {
+                echo 'class="active"';
+            } ?>><a href="index.php">Início</a></li>
+        <li <?php if ($activePage == "meuscontratos") {
+                echo 'class="active"';
+            } ?>><a href="meuscontratos.php">Meus Contratos</a></li>
+        <li <?php if ($activePage == "help") {
+                echo 'class="active"';
+            } ?>><a href="help.php">Como reservar</a></li>
     </ul>
 </nav>
